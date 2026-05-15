ffc39227801790a8fb677293b68c1a7e043b6b45
R3p0 <139224742+omareladawi@users.noreply.github.com>
2026-05-15 17:51:44 +0300

    Delete Project directory

--- Project/src/web_scanner/reporting/__init__.py
+++
@@ -1,3 +0,0 @@
-from .report_generator import generate_report
-
-__all__ = ['generate_report']
--- Project/src/web_scanner/scanner/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Scanner module initialization."""
-from .vulnerability_scanner import VulnerabilityScanner
-
-__version__ = '0.1.0'
-__all__ = [
-    'VulnerabilityScanner'
-]
